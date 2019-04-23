b176d958c4880206b0540bc803dd285b400d18c4
Sam Wicks <c3331974@uon.edu.au>
2019-04-23 04:05:05 +0000

    This commit involved writing code for getting the word with user input

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+
+
 int main()
 {
-
+    //This block of code will be used to obtain user input for the program.
+    printf("How many letters do you have in your word? ");
+    int limit;
+    scanf("%d", &limit);
+    char word[limit]; //This is the string for user input and will be used for either the rotation cipher or subsitution cipher.
+    printf("What is your word? ");
+    scanf('%s', &word);
+    
+    
 }
