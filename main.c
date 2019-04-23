30faba1858bc7b8c003fa3d702d884fb83f0090f
Sam Wicks <c3331974@uon.edu.au>
2019-04-23 10:40:28 +0000

    .

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int Rotation_Cipher(word);
-int Subsitution_Cipher(word);
+void Rotation_Cipher(char word);
+void Subsitution_Cipher(char word);
 
 int main()
 {
     //This block of code will be used to obtain user input for the program.
-    printf("How many letters do you have in your word? ");
     int limit;
-    scanf("%d", &limit);
     char word[limit]; //This is the string for user input and will be used for either the rotation cipher or subsitution cipher.
     printf("What is your word? ");
     scanf('%s', &word);
+    printf("How many letters do you have in your word? ");
+    scanf("%d", &limit);
+    
     
     //This block of code will be used to to determin which cipher the user wants to use.
     printf("What Cipher do you want? (1) Rotation Cipher.... (2) Subsitution Cipher");
@@ -22,10 +23,29 @@ int main()
         case 1:
         Rotation_cipher(word);
         case 2:
-        Subsition_cipher(word);
+        Subsitution_cipher(word);
         default:
         printf("Incorrect choice. Restart program.");
         
     }
 
 }
+
+void Rotation_cipher(char word)
+{
+    
+}
+
+void Subsitution_cipher(char word)
+{
+    
+}
+
+
+
+
+
+
+
+
+
