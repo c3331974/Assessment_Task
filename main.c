7ce800018883df5c1e7ba7d4bf2516e5b37f20a4
sam wicks <sam.wicks456@gmail.com>
2019-04-24 01:03:04 +0000

    Changed the switch statement to if else statements. Did some changing around to stop the "segmentation fault" message and "killed" message.

--- main.c
+++ main.c
@@ -5,31 +5,33 @@ void Subsitution_Cipher(char word);
 
 int main()
 {
+top:
     //This block of code will be used to obtain user input for the program.
-    int limit;
-    char word[limit]; //This is the string for user input and will be used for either the rotation cipher or subsitution cipher.
-    printf("What is your word? ");
-    scanf('%s', &word);
     printf("How many letters do you have in your word? ");
+    int limit;
     scanf("%d", &limit);
+    char word[limit]; //This is the string for user input and will be used for either the rotation cipher or subsitution cipher.
+    printf("What is your word: ");
+    scanf("%s", word);
     
     
     //This block of code will be used to to determin which cipher the user wants to use.
     printf("What Cipher do you want? (1) Rotation Cipher.... (2) Subsitution Cipher");
     int decision;
     scanf("%d", &decision);
-    switch(decision)
-    {
-        case 1:
+    
+    if (decision == 1){
         Rotation_cipher(word);
-        case 2:
+        return 0;
+    }else if(decision == 2){
         Subsitution_cipher(word);
-        default:
-        printf("Incorrect choice. Restart program.");
-        
+        return 0;
+    }else{
+        printf("Incorrect choice. Restart program.\n");
+        goto top;
     }
-
 }
+
 
 void Rotation_cipher(char word)
 {
